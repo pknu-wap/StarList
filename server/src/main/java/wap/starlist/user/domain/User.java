7934d6677a0143ec5e53fd2aeb6b2a4466635947
gyunho <gyunh01@pukyong.ac.kr>
2025-04-11 22:12:29 +0900

    refactor: OAuth2에서 제공하는 정보를 저장하도록 User 변경

--- server/src/main/java/wap/starlist/user/domain/User.java
+++ server/src/main/java/wap/starlist/user/domain/User.java
@@ -1,24 +1,38 @@
 package wap.starlist.user.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import wap.starlist.config.auth.dto.UserRole;
 
+@Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String name;
     private String email;
     private String password;
-    private String name;
-
+    private String profileImage;
+    private String provider;    // OAuth2 로그인 제공자 (ex: google, kakao)
+    private String providerId;  // OAuth2 제공자가 발급한 유저 고유 id
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
-    // OAuth2 로그인 제공자 (ex: google, kakao)
-    private String provider;
+    public User update(String name, String profileImage) {
+        this.name = name;
+        this.profileImage = profileImage;
+        return this;
+    }
 
-    // OAuth2 제공자가 발급한 유저 고유 id
-    private String providerId;
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
--- server/src/main/java/wap/starlist/user/domain/UserRole.java
+++
@@ -1,6 +0,0 @@
-package wap.starlist.user.domain;
-
-public enum UserRole {
-    ADMIN,
-    USER
-}
