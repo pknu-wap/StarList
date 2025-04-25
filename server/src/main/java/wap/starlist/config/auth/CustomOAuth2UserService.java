1a56d1c66aae497b64362885c554f9ba6cffe98e
gyunho <gyunh01@pukyong.ac.kr>
2025-04-25 21:51:03 +0900

    refactor: db에 값을 변경하는 메서드에서 트랜잭션을 보장하도록 변경

--- server/src/main/java/wap/starlist/config/auth/CustomOAuth2UserService.java
+++ server/src/main/java/wap/starlist/config/auth/CustomOAuth2UserService.java
@@ -10,7 +10,8 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import wap.starlist.config.auth.dto.OAuthAttributes;
+import org.springframework.transaction.annotation.Transactional;
+import wap.starlist.config.auth.dto.OAuth2Response;
 import wap.starlist.config.auth.dto.SessionUser;
 import wap.starlist.member.domain.Member;
 import wap.starlist.member.repository.MemberRepository;
@@ -25,6 +26,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     private final HttpSession httpSession;
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
