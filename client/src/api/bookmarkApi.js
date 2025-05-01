20d71dc75fbd9e3fda01f7882dc807cfa54cd1b4
SH-MooDy <shlee4167@naver.com>
2025-05-01 14:07:52 +0900

    chore: 로그인 로직 변경

--- client/src/api/bookmarkApi.js
+++ client/src/api/bookmarkApi.js
@@ -1,20 +1,9 @@
-import axios from 'axios';
+import instance from "./axiosInstance";
 
-const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
-    withCredentials: true,
-});
-
-// 북마크 API 요청 함수
 export const fetchBookmarks = async () => {
     try {
         const res = await instance.get("/bookmarks");
-        if (Array.isArray(res.data.data)) {
-            return res.data.data;
-        }
-
-        console.warn("'data'가 배열이 아님:", res.data.data);
-        return [];
+        return Array.isArray(res.data.data) ? res.data.data : [];
     } catch (error) {
         console.error("북마크 불러오기 실패:", error);
         return [];
