194178030fd0735d63b11f2668e9d1b216403ef4
SH-MooDy <shlee4167@naver.com>
2025-05-13 15:47:16 +0900

    feature: 주로 사용하는 색상 팔레트 tailwind.config.js에 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,26 @@ export default {
     content: ["./index.html", "./src/**/*.{js,jsx,html}"],
 
     theme: {
+        colors: {
+            Main: {
+                Black: "#1A1A1A",
+                600: "#57418B",
+                500: "#7349D6",
+                400: "#966AFF",
+                300: "AD93FF",
+                200: "C8B8FF",
+                100: "E4DBFF",
+            },
+            Gray: {
+                600: "373542",
+                500: "4D4A57",
+                400: "8D87A2",
+                300: "C8C3D6",
+                200: "DDD9E7",
+                100: "E8E6F0",
+            },
+
+        },
         extend: {
             fontFamily: {
                 pretendard: ["Pretendard", "sans-serif"],
