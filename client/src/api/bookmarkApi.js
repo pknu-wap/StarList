8ef5061df94994865817067f147f388c79b080a7
SH-MooDy <shlee4167@naver.com>
2025-05-17 19:34:48 +0900

    fix: 토큰 저장 관련 문제 해결

--- client/src/api/bookmarkApi.js
+++ client/src/api/bookmarkApi.js
@@ -1,9 +1,16 @@
 import instance from "./axiosInstance";
-
+import useAuthStore from "../store/useAuthStore";
 
 export const fetchBookmarks = async () => {
+    // Zustand 스토어에서 토큰 가져오기
+    const token = useAuthStore.getState().accessToken;
+
     try {
-        const res = await instance.get("/bookmarks");
+        const res = await instance.get("/bookmarks", {
+            headers: {
+                Authorization: token ? `Bearer ${token}` : undefined,
+            },
+        });
         return Array.isArray(res.data.data) ? res.data.data : [];
     } catch (error) {
         console.error("북마크 불러오기 실패:", error);
