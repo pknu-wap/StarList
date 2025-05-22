7948fec77646dc58f799a8d9e54e192fed6eb1f1
MNSK-00 <mskim345678@gmail.com>
2025-05-22 21:50:39 +0900

    fix: API 호출시 토큰 가져오기 로직 수정

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import useAuthStore from "../functions/hooks/useAuthStore";
+
+// 로그인, 로그아웃 관련 Context 생성
+const AuthContext = createContext();
+
+// 해당 Context 를 제공하는 컴포넌트
+const AuthProvider = ({ children }) => {
+    const { accessToken, login, logout } = useAuthStore();
+    const [isLoggedIn, setIsLoggedIn] = useState(!!accessToken);
+
+    // Zustand store의 token 변화에 따라 isLoggedIn 상태 동기화
+    useEffect(() => {
+        setIsLoggedIn(!!accessToken);
+    }, [accessToken]);
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, accessToken, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+// Context 사용을 위한 Hook
+const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  // 만약 Context Provider 바깥에서 이 Hook 을 호출하면 에러를 반환
+  if (!context)
+    throw new Error('useAuth must be used within AuthProvider');
+
+  return context;
+};
+
+export { AuthProvider, useAuth };
