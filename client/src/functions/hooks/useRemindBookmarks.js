88729a3ad4645ce3b7f7cfa5b9650bd9c7f94fac
SH-MooDy <shlee4167@naver.com>
2025-05-23 22:33:06 +0900

    chore: 지속 fetch 시도 오류 해결

--- client/src/functions/hooks/useRemindBookmarks.js
+++ client/src/functions/hooks/useRemindBookmarks.js
@@ -6,6 +6,7 @@ const useRemindBookmarks = () => {
         queryKey: ['remindBookmarks'],
         queryFn: getRemindBookmarks,
         staleTime: 1000 * 60, // 1분 동안 캐싱
+        retry: 0, // 실패 시 재시도 X
     });
 }
 export default useRemindBookmarks;
