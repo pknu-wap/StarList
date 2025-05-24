511f4c1b60a0b5b1360fb826670b9e07595e88d0
MNSK-00 <mskim345678@gmail.com>
2025-05-24 21:49:30 +0900

    chore: import 경로 오타 수정

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/header/Header";
-import CardList from "../components/cardlist/CardList";
+import CardList from "../components/Cardlist/CardList";
 import RemindCarousel from "../components/remainder/RemindCarousel";
 
 const MainPage = () => {
