684c1e85abde1e4934092377d5a8b57f5bcb0c3a
gyunho <gyunh01@pukyong.ac.kr>
2025-05-24 17:37:47 +0900

    feat: 에러 응답 enum 생성

---
+++ server/src/main/java/wap/starlist/error/ErrorCode.java
@@ -0,0 +1,19 @@
+package wap.starlist.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "1001", "잘못된 HTTP 메서드를 호출했습니다."),
+
+    NOT_FOUND(HttpStatus.NOT_FOUND, "3000", "존재하지 않는 엔티티입니다."),
+    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "3001", "북마크가 존재하지 않습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
