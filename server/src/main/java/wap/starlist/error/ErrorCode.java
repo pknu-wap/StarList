df9e268482c6f188cc6dbccee2fee3bc270b0517
gyunho <gyunh01@pukyong.ac.kr>
2025-05-25 17:30:00 +0900

    feat: 리프레시 토큰이 만료되었을 때 에러 코드 추가

--- server/src/main/java/wap/starlist/error/ErrorCode.java
+++ server/src/main/java/wap/starlist/error/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "1001", "잘못된 HTTP 메서드를 호출했습니다."),
 
-    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "2001", "토큰이 만료되었습니다."),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "2001", "액세스 토큰이 만료되었습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "2002", "리프레시 토큰이 만료되었습니다."),
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "3000", "존재하지 않는 엔티티입니다."),
     TOP_FOLDERS_NOT_FOUND(HttpStatus.NOT_FOUND, "3001", "최상위 폴더가 존재하지 않습니다."),
