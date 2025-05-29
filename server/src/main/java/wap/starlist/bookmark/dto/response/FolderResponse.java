bc58e109eb81306e5614424949b7ef919c0994bb
NamEon <143885031+skadjs@users.noreply.github.com>
2025-05-29 21:44:08 +0900

    refactor: 폴더 생성 응답 반환값 수정
    - id 외에 클라이언트 호출 시 필요한 정보들을 추가함.

--- server/src/main/java/wap/starlist/bookmark/dto/response/FolderResponse.java
+++ server/src/main/java/wap/starlist/bookmark/dto/response/FolderResponse.java
@@ -10,12 +10,28 @@ public class FolderResponse {
 
     private Long folderId;
 
-    private String message;
+    private String title;
+
+    private Long googleId;
+
+    private Integer position;
+
+    private String folderType;
+
+    private Long dateAdded;
+
+    private Long dateGroupModified;
 
     // Folder를 전달받아 response 객체를 생성하는 정적 팩토리 메서드
     public static FolderResponse from(Folder folder) {
         return FolderResponse.builder()
                 .folderId(folder.getId())
+                .title(folder.getTitle())
+                .googleId(folder.getGoogleId())
+                .position(folder.getPosition())
+                .folderType(folder.getFolderType())
+                .dateAdded(folder.getDateAdded())
+                .dateGroupModified(folder.getDateGroupModified())
                 .build();
     }
 }
