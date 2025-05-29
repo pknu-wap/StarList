54fb748497b398e16a6ef75bff1b28f88ef72218
MNSK-00 <mskim345678@gmail.com>
2025-05-29 19:46:06 +0900

    feature: 검색을 위한 훅 추가 및 적용

---
+++ client/src/functions/utils/getNodesBySearch.js
@@ -0,0 +1,9 @@
+import fetchNodesByPath from "./fetchNodesByPath";
+
+async function getNodesBySearch(keyword) {
+    // 한글, 특수문자 등은 URL 에서 그대로 쓸 수 없으므로 인코딩
+    const query = encodeURIComponent(keyword);
+    return await fetchNodesByPath(`/bookmarks/search?query=${query}`);
+}
+
+export default getNodesBySearch;
