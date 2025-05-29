98771cce7ee556d4e59420e667317d7cf70fd640
SH-MooDy <shlee4167@naver.com>
2025-05-29 14:44:30 +0900

    feat: 디버깅용 콘솔 로그 추가

--- client/src/components/modal/DropDown.jsx
+++ client/src/components/modal/DropDown.jsx
@@ -27,7 +27,7 @@ const DropDown = ({ options, selected, setSelected }) => {
         setSelected(node);
         setIsOpen(false);
     };
-
+    console.log("DropDown options", options);
     const renderTree = (nodes, level = 0) =>
         nodes.map((node) => {
             const hasChildren = Array.isArray(node.children) && node.children.length > 0;
