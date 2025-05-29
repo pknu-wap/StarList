2bcf0ed1c762ff93de79070a342d873761fef1d6
SH-MooDy <shlee4167@naver.com>
2025-05-30 01:55:43 +0900

    임시저장

--- client/src/components/cardlist/AddButton.jsx
+++ client/src/components/cardlist/AddButton.jsx
@@ -35,23 +35,18 @@ const AddButton = () => {
     return (
         <div className="relative inline-block" ref={buttonRef}>
             <button
-    className="w-[120px] h-[43px] relative"
-    style={{
-        filter: "drop-shadow(0px 4px 14.9px rgba(0,0,0,0.25))",
-    }}
-    onClick={() => setOpenMenu(v => !v)}
->
-    {/* 배경 */}
-    <div className="w-[120px] h-[43px] absolute left-[-1.5px] top-[-1.5px] rounded-[78px] bg-gradient-to-r from-main-500 to-main-black z-0" />
-    
-    {/* + 아이콘 */}
-    <PlusIcon className="absolute left-6 top-[11px] w-5 h-5 text-white z-10" />
-
-    {/* New 텍스트 */}
-    <p className="w-[34px] h-[22px] absolute left-[57px] top-2.5 text-base font-medium text-center text-white z-10 flex items-center justify-center">
-        New
-    </p>
-</button>
+                className="w-[120px] h-[43px] relative"
+                style={{
+                    filter: "drop-shadow(0px 4px 14.9px rgba(0,0,0,0.25))",
+                }}
+                onClick={() => setOpenMenu(v => !v)}
+            >
+                <div className="w-[120px] h-[43px] absolute left-[-1.5px] top-[-1.5px] rounded-[78px] bg-gradient-to-r from-main-500 to-main-black z-0"></div>
+                <PlusIcon className="absolute left-6 top-[11px] w-5 h-5 text-white z-10" />
+                <p className="w-[34px] h-[22px] absolute left-[57px] top-2.5 text-base font-medium text-center text-white z-10 flex items-center justify-center">
+                    New
+                </p>
+            </button>
             {openMenu && (
                 <AddSelectMenu
                     onSelect={handleSelect}
