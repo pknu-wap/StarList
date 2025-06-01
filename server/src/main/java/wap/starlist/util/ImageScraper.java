970ea741fd4b9c3218ed3f9d9cb2eacf027b5c78
gyunho <gyunh01@pukyong.ac.kr>
2025-06-01 16:22:44 +0900

    refactor: og 태그 이미지를 가져오도록 변경

--- server/src/main/java/wap/starlist/util/ImageScraper.java
+++ server/src/main/java/wap/starlist/util/ImageScraper.java
@@ -19,4 +19,28 @@ public class ImageScraper {
         return Optional.ofNullable(thumbnail)
                 .map(img -> img.absUrl("src"));
     }
+
+    public static Optional<String> getImageFromOgOrImgTag(String url) throws IOException {
+        if (url == null || url.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Document doc = Jsoup.connect(url).get();
+
+        Element ogImage = doc.select("meta[property=og:image]").first();
+        if (ogImage == null) {
+            // 없으면 img 태그에서 스크랩
+            Element thumbnail = doc.select("img").first();
+            return Optional.ofNullable(thumbnail)
+                    .map(img -> img.absUrl("src"));
+        }
+
+        // og:image 스크랩
+        String content = ogImage.attr("content");
+        if (content != null && !content.isEmpty()) {
+            // 절대경로 보장
+            return Optional.of(doc.absUrl("base").isEmpty() ? content : doc.absUrl("base") + content);
+        }
+        return Optional.empty();
+    }
 }
