7d919fd5cd3c9f79b4f3fbbd10c98d3e27caf624
gyunho <gyunh01@pukyong.ac.kr>
2025-06-01 17:36:33 +0900

    refactor: 부모 폴더 내에 북마크가 생성되도록 변경

--- server/src/main/java/wap/starlist/bookmark/service/BookmarkService.java
+++ server/src/main/java/wap/starlist/bookmark/service/BookmarkService.java
@@ -19,10 +19,12 @@ import org.springframework.util.StopWatch;
 import wap.starlist.bookmark.domain.Bookmark;
 import wap.starlist.bookmark.domain.Folder;
 import wap.starlist.bookmark.domain.Root;
+import wap.starlist.bookmark.dto.request.BookmarkCreateRequest;
 import wap.starlist.bookmark.dto.request.BookmarkTreeNode;
 import wap.starlist.bookmark.repository.BookmarkRepository;
 import wap.starlist.bookmark.repository.FolderRepository;
 import wap.starlist.bookmark.repository.RootRepository;
+import wap.starlist.error.exception.FolderNotFoundException;
 import wap.starlist.util.ImageScraper;
 
 @Slf4j
@@ -37,7 +39,11 @@ public class BookmarkService {
     private final RootRepository rootRepository;
 
     @Transactional // 트랜잭션을 보장하기 위해
-    public Bookmark createBookmark(String memberProviderId, String title, String url) {
+    public Bookmark createBookmark(String memberProviderId, BookmarkCreateRequest request) {
+        String title = request.getTitle();
+        String url = request.getUrl();
+        Long folderId = request.getFolderId();
+
         // 중복 확인
         List<Bookmark> found = bookmarkRepository.findByUrl(url);
 
@@ -51,13 +57,21 @@ public class BookmarkService {
         // 웹 페이지의 이미지 파싱
         String imgUrl = scrapImageOrEmpty(url);
 
+        // 폴더 확인
+        Folder folder = folderRepository.findById(folderId)
+                .orElseThrow(FolderNotFoundException::new);
+
         // 전달받은 값으로 임시 북마크 생성
         Bookmark bookmark = Bookmark.builder()
                 .title(title)
                 .url(url)
                 .image(imgUrl)
                 .dateAdded(currentTime())
+                .folder(folder) // 북마크 -> 폴더 연관관계 설정
                 .build();
+
+        // 폴더 -> 북마크 연관관계 설정, folder는 영속 상태이기 때문에 save하지 않아도 됨
+        folder.addChildBookmark(bookmark);
 
         // 북마크 저장
         return bookmarkRepository.save(bookmark);
