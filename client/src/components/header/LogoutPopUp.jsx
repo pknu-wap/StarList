ab0233f478f18013c1caa6c8d7519cab3e401522
MNSK-00 <mskim345678@gmail.com>
2025-06-04 03:35:07 +0900

    fix: 카드리스트 mapping 오류 해결 및 로그아웃 팝업 CSS 수정

--- client/src/components/header/LogoutPopUp.jsx
+++ client/src/components/header/LogoutPopUp.jsx
@@ -1,32 +1,43 @@
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useCallback, useRef } from "react";
 import useAuthStore from "../../functions/hooks/useAuthStore";
 import { Logout } from "../../assets";
 
-const LogoutPopUp = () => {
+const LogoutPopUp = ({ onClose }) => {
     const logout = useAuthStore((s) => s.logout);
-    const navigate = useNavigate();
+    const popupRef = useRef(null);
 
     const handleLogout = () => {
         logout();
-        navigate("/start", { replace: true });
     };
 
+    // 현재 영역을 벗어난 곳을 클릭하면 모달창 닫기
+    const handleClickOutside = useCallback(
+        (e) => {
+            if (popupRef.current && !popupRef.current.contains(e.target)) onClose();
+        },
+        [onClose],
+    );
+
+    // 마운트될때 mousedown 이벤트 리스너를 등록, 언마운트시 해제
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [handleClickOutside]);
+
     return (
-        <div className="w-[377px] h-[80px] rounded-[20px] bg-white shadow-card relative flex justify-center items-center">
+        <div
+            ref={popupRef}
+            className="absolute right-14 top-14 flex w-[370px] items-center justify-center rounded-2xl bg-white p-4 shadow-card"
+        >
             <div
-                className="
-                    w-[337px] h-[60px] 
-                    rounded-2xl bg-gray-100 hover:bg-gray-200
-                    flex flex-col justify-center items-center
-                    cursor-pointer
-                "
+                className="flex w-[330px] cursor-pointer flex-col items-center justify-center rounded-2xl bg-gray-100 p-4 hover:bg-gray-200"
                 onClick={handleLogout}
             >
-                <div className="flex">
-                    <Logout className="w-8 h-8 pr-[10px] pb-1" />
-                    <p className="text-xl font-medium text-black font-sans">
-                        로그아웃
-                    </p>
+                <div className="flex items-center gap-x-2">
+                    <Logout className="h-8 w-8" />
+                    <p className="font-sans text-xl font-medium text-black">로그아웃</p>
                 </div>
             </div>
         </div>
