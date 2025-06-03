0b70735c4595cf52ee58385da90fb916958c3098
SH-MooDy <shlee4167@naver.com>
2025-06-03 17:27:31 +0900

    chore:메인페이지 수정

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -6,7 +6,7 @@ import ScrollToTopButton from "../components/cardlist/ScrollToTopButton";
 
 const MainPage = () => {
     return (
-        <div className="grid grid-rows-[1fr_3fr_6fr] gap-y-6 px-4 py-2">
+        <div className="grid gap-y-6 px-4 py-2">
             <Header />
             <RemindCarousel />
             <CardList />
